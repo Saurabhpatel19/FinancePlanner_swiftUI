9929ac1995ca5a5791ee3aa3be253bf4d892102c
saurabh patel <saurabh@avdevs.com>
2025-12-25 15:10:07 +0530

    Initial Commit

---
+++ FinancePlanner/FinancePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancePlannerApp.swift
+//  FinancePlanner
+//
+//  Created by Saurabh on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FinancePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
